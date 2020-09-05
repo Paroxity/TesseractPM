287b6a3b0b0dcab8c9065797021ed74dbb791375
TwistedAsylumMC <twistedasylummc@gmail.com>
2020-09-05 09:39:32 +0100

    Allow localhost & local docker connections to join

--- src/paroxity/tesseract/EventListener.php
+++ src/paroxity/tesseract/EventListener.php
@@ -3,7 +3,8 @@
 namespace paroxity\tesseract;
 
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerPreLoginEvent;
+use function preg_match;
 
 class EventListener implements Listener
 {
@@ -15,14 +16,11 @@ class EventListener implements Listener
         $this->plugin = $plugin;
     }
 
-    public function onPlayerJoin(PlayerJoinEvent $event): void
+    public function onPlayerPreLogin(PlayerPreLoginEvent $event): void
     {
-        $player = $event->getPlayer();
-        $session = $player->getNetworkSession();
-
         $ip = $this->plugin->getProxyAddress();
-        if ($session->getIp() !== $ip) {
-            $player->transfer($this->plugin->getProxyAddress(), $this->plugin->getProxyPort());
+        if ($event->getIp() !== $ip && !preg_match("/^(127|172)/", $event->getIp())) {
+            $event->setKickReason(PlayerPreLoginEvent::KICK_REASON_PLUGIN, "You must join via the proxy");
         }
     }
 }

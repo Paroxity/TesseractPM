a2c3a170d09878d5c45ec0c574588e677ea2885c
TwistedAsylumMC <twistedasylummc@gmail.com>
2020-09-11 00:31:59 +0100

    Fix issues with auto-reconnecting to the socket server

--- src/paroxity/tesseract/thread/SocketThread.php
+++ src/paroxity/tesseract/thread/SocketThread.php
@@ -12,15 +12,19 @@ use pocketmine\thread\Thread;
 use pocketmine\utils\Binary;
 use Threaded;
 use function sleep;
+use function socket_close;
 use function socket_connect;
 use function socket_create;
-use function socket_last_error;
-use function socket_read;
+use function socket_recv;
 use function socket_set_nonblock;
 use function socket_write;
 use function strlen;
+use function usleep;
+use function var_dump;
 use const AF_INET;
 use const IPPROTO_IP;
+use const MSG_DONTWAIT;
+use const MSG_WAITALL;
 use const PTHREADS_INHERIT_NONE;
 use const SOCK_STREAM;
 
@@ -71,31 +75,35 @@ class SocketThread extends Thread
     {
         $socket = $this->connectToSocketServer();
         socket_set_nonblock($socket);
+
         while ($this->isRunning) {
             while (($send = $this->sendQueue->shift()) !== null) {
                 $length = strlen($send);
-                socket_write($socket, Binary::writeLInt($length) . $send, 4 + $length);
-            }
-            do {
-                $lengthBuf = @socket_read($socket, 4);
-                if (!$lengthBuf && socket_last_error($socket) === 10054) {
+                $wrote = @socket_write($socket, Binary::writeLInt($length) . $send, 4 + $length);
+                if($wrote === 0){
+                    socket_close($socket);
                     $socket = $this->connectToSocketServer();
                 }
-                if ($lengthBuf !== false) {
-                    try {
-                        $length = Binary::readLInt($lengthBuf);
-                        $buffer = socket_read($socket, $length);
-                        if ($buffer !== false) {
-                            $this->receiveBuffer[] = $buffer;
-                            $this->notifier->wakeupSleeper();
-                        }
-                    } catch (Exception $exception) {
-                        echo "Failed to read from socket: " . ($exception->getMessage()) . PHP_EOL;
+            }
+
+            do {
+                $read = @socket_recv($socket, $lengthBuf, 4, MSG_DONTWAIT);
+                if ($read === 4) {
+                    $length = Binary::readLInt($lengthBuf);
+                    $read = @socket_recv($socket, $buffer, $length, MSG_WAITALL);
+                    if ($buffer !== false) {
+                        $this->receiveBuffer[] = $buffer;
+                        $this->notifier->wakeupSleeper();
                     }
+                } else if($read === 0){
+                    socket_close($socket);
+                    $socket = $this->connectToSocketServer();
                 }
-            } while ($lengthBuf !== false && $lengthBuf !== "");
+            } while ($read !== 0);
             usleep(200);
         }
+
+        socket_close($socket);
     }
 
     /**
@@ -104,10 +112,9 @@ class SocketThread extends Thread
      */
     public function connectToSocketServer()
     {
-        $socket = socket_create(AF_INET, SOCK_STREAM, IPPROTO_IP);
-        if (!$socket) {
-            throw new Exception("Unable to start socket client");
-        }
+        do {
+            $socket = socket_create(AF_INET, SOCK_STREAM, IPPROTO_IP);
+        } while (!$socket);
 
         do {
             $connected = @socket_connect($socket, $this->host, $this->port);
